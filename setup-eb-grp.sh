3deff68d8d1ae86e52eb7cc67bb76cdc68c80d77
Tomas Kazmar <tomash.kazmar@seznam.cz>
2017-10-24 12:29:48 +0200

    Patch easybuild setup on II2.

--- setup-eb-grp.sh
+++ setup-eb-grp.sh
@@ -5,9 +5,8 @@ function log_setup_eb_grp_sh ()
 	echo >&2 "$(date +%Y%m%d%H%M%S) [~/etc/setup-eb-grp.sh $GRP]: $1"
 }
 
-source ~/etc/setup-modules-grp.sh "$GRP"
-
 if [ -d /biosw ]; then
+	source ~/etc/setup-modules-grp.sh "$GRP"
 	ml load EasyBuild/3.1.0
 	log_setup_eb_grp_sh "Setting up environment variables."
 	#export PYTHONPATH=$PYTHONPATH:$HOME/eb-mns
@@ -25,10 +24,16 @@ elif [ "$IMPIMBA_MACHINE_NAME" == "IMPIMBA-2" ]; then
 	if [ "$EASYBUILD_PREFIX" != "/software/$EB_EDITION" ]; then
 		echo >&2 "Expected eb module tree at /software/$EB_EDITION. Quitting."
 	else
+		ml purge
+		ml load build-env/$EB_EDITION
 		ml load easybuild/3.4.1
+		module use "/groups/$GRP/software/$EB_EDITION-ii2/modules/all"
 		log_setup_eb_grp_sh "Setting up environment variables."
+		export EASYBUILD_ROBOT_PATHS=/software/$EB_EDITION/ebfiles_repo/:/groups/pauli/kazmar/projs/eb/ii2/easybuild/easyconfigs
 		export EASYBUILD_BUILDPATH=/tmp
 		export EASYBUILD_PREFIX=/groups/$GRP/software/$EB_EDITION-ii2/
+		export EASYBUILD_GROUP_WRITABLE_INSTALLDIR=${GRP}.GRP
+		newgrp ${GRP}.GRP
 	fi
 	unset EB_EDITION
 else
